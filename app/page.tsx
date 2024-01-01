b651b0fbad9a619a776d753c351e4c358479f79c
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-01-02 01:50:25 +0900

    done today

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
 import Link from "next/link";
 import { currentUser } from "@clerk/nextjs";
+import { RocketIcon } from "lucide-react";
+import { useState } from "react";
 
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
 
 export default async function Home() {
   const user = await currentUser();
@@ -12,6 +26,15 @@ export default async function Home() {
   return (
     <>
       <Navbar />
+      <div className="flex justify-center mt-5">
+        <Alert className="w-1/2">
+          <RocketIcon className="w-5 h-5" />
+          <AlertTitle>Welcome to CourseManger, {user?.firstName}!</AlertTitle>
+          <AlertDescription>
+            Register your TUID to access the functionalities.
+          </AlertDescription>
+        </Alert>
+      </div>
       <p className="text-xl text-center m-5">Your Role: {role}</p>
       {metadata?.hasRegistered ? (
         <div className="text-center">
@@ -30,10 +53,14 @@ export default async function Home() {
           <Button variant="temple">Manage users - admin</Button>
         </Link>
         <Link href="/management">
-          <Button variant="temple">Add/Update/Delete a course - coordinator</Button>
+          <Button variant="temple">
+            Add/Update/Delete a course - coordinator
+          </Button>
         </Link>
         <Link href="/cancellation">
-          <Button variant="temple">Cancel a course - coordinator & faculty</Button>
+          <Button variant="temple">
+            Cancel a course - coordinator & faculty
+          </Button>
         </Link>
         <Link href="/reports">
           <Button variant="temple">Check all the courses - staff</Button>
