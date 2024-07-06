94a67c7f292b259afa271298766ecb3951c1fd96
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-07-07 02:59:34 +0900

    completed keycloak setupgit add .

--- app/user-management/page.tsx
+++ app/user-management/page.tsx
@@ -2,27 +2,38 @@ import UserTable from "./_components/UserTable";
 
 import getAllUsers from "../actions/getAllUsers";
 import getCurrentUserFromDb from "../actions/getCurrentUserFromDb";
+import { auth } from "@/auth";
 
 const UserManagementPage = async () => {
   const users = await getAllUsers();
   const currentUser = await getCurrentUserFromDb();
 
-  const isAdmin = currentUser?.isAdmin;
-  const isCoordinator = currentUser?.isCoordinator;
+  const session = await auth();
+
+  const isAdmin = session?.user.roles.includes("admin");
+  const isCoordinator = session?.user.roles.includes("coordinator");
 
   return (
     <>
-      <div className={`max-w-[1800px] pt-[85px] px-10 ${(!isAdmin && !isCoordinator) && "hidden"}`}>
+      <div
+        className={`max-w-[1800px] pt-[85px] px-10 ${
+          !isAdmin && !isCoordinator && "hidden"
+        }`}
+      >
         <h1 className="text-2xl font-bold">Manage Users</h1>
         <p className="text-slate-500 mt-1">
-          After users sign up, they will be &apos;guest&apos; users, who have no roles.
-          Please add roles to them.
+          After users sign up, they will be &apos;guest&apos; users, who have no
+          roles. Please add roles to them.
         </p>
         <div className="mt-5">
           <UserTable users={users} currentUser={currentUser} />
         </div>
       </div>
-      <div className={`h-full flex items-center justify-center ${(isAdmin || isCoordinator) && "hidden"}`}>
+      <div
+        className={`h-full flex items-center justify-center ${
+          (isAdmin || isCoordinator) && "hidden"
+        }`}
+      >
         You cannot access this.
       </div>
     </>
--- lib/types.ts
+++ lib/types.ts
@@ -20,3 +20,35 @@ export type ScheduleType = {
     end: string;
   };
 };
+
+export type DecodedType = {
+  exp: number;
+  iat: number;
+  auth_time: number;
+  jti: string;
+  iss: string;
+  aud: string;
+  sub: string;
+  typ: string;
+  azp: string;
+  sid: string;
+  acr: string;
+  allowed_origins: string[];
+  realm_access: {
+    roles: string[];
+  };
+  resource_access: {
+    [key: string]: {
+      roles: string[];
+    };
+  };
+  scope: string;
+  upn: string;
+  email_verified: boolean;
+  name: string;
+  groups: string[];
+  preferred_username: string;
+  given_name: string;
+  family_name: string;
+  email: string;
+};
