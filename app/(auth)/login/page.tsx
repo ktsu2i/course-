89eef5c7d8ce4281d2d7835f02d579cb501f7f14
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-08-24 00:43:38 +0900

    fixed the position of login card

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,15 +1,56 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { login } from "../../actions/login";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
   return (
-    <div>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
-        <button formAction={login}>Log in</button>
-      </form>
+    // <div>
+    //   <form>
+    //     <label htmlFor="email">Email:</label>
+    //     <input id="email" name="email" type="email" required />
+    //     <label htmlFor="password">Password:</label>
+    //     <input id="password" name="password" type="password" required />
+    //     <button formAction={login}>Log in</button>
+    //   </form>
+    // </div>
+    <div className="h-full flex justify-center items-center">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to Course Manager</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  placeholder="example@temple.edu"
+                  type="email"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" placeholder="Password" type="password" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button variant="temple">Login</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
