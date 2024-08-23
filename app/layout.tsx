13e776f92809d0ba731126c55a4e5d73ca81f251
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-08-23 20:14:29 +0900

    removed unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,5 @@
 import "./globals.css";
 
-import ToasterProvider from "@/components/providers/ToasterProvider";
-import Sidebar from "@/components/sidebar/Sidebar";
-import Navbar from "@/components/Navbar";
-
 export default function RootLayout({
   children,
 }: {
