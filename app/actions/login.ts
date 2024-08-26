fb8852e43ab76e8dd00bb75f396226ebcfacd598
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-08-26 14:46:17 +0900

    debugging

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -8,7 +8,13 @@ import { createClient } from "@/utils/supabase/server";
 export async function login(email: string, password: string) {
   const supabase = createClient();
 
-  const { error } = await supabase.auth.signInWithPassword({ email, password });
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  const token = data.session?.access_token;
+  console.log(token);
 
   if (error) {
     redirect("/error");
