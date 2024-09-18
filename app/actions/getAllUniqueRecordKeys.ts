bbbfb8d30fa9b4b51c918dbed062c69d899a07ee
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-09-19 01:38:07 +0900

    getAllUniqueRecordKeys

--- app/actions/getAllUniqueRecordKeys.ts
+++ app/actions/getAllUniqueRecordKeys.ts
@@ -1,12 +1,14 @@
-import { db } from "@/lib/db";
+// import { db } from "@/lib/db";
 
 export default async function getAllUniqueRecordKeys() {
   try {
-    const groupedRecordKeys = await db.course.groupBy({
-      by: ["recordKey"],
-    });
+    // const groupedRecordKeys = await db.course.groupBy({
+    //   by: ["recordKey"],
+    // });
 
-    const recordKeys = groupedRecordKeys.map((group) => group.recordKey);
+    // const recordKeys = groupedRecordKeys.map((group) => group.recordKey);
+
+    const recordKeys: string[] = [];
 
     return recordKeys;
     
